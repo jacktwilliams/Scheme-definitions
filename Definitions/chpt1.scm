1ab1e76a6642ca2b1f0a1403028bfbf57e411914
Jack Williams <jack.thomas.mail@gmail.com>
2018-06-25 14:01:50 -0500

    Added definitions directory for code that is copied from book. Moved cubic root procedure into excersises.

---
+++ Definitions/chpt1.scm
@@ -0,0 +1,35 @@
+#lang sicp
+(define (square x)
+  (* x x))
+
+(define (avg x y)
+ (/ (+ x y) 2))
+
+(define (abs x)
+ (if (< x 0) (- x)
+     x))
+
+(define (improve x guess)
+ (avg guess (/ x guess)))
+
+(define (good-enough? guess prev-guess)
+ (< (abs (- guess prev-guess)) .0001))
+
+(define (sqrt-iter x guess prev-guess)
+  (if (good-enough? guess prev-guess)
+      guess
+      (sqrt-iter x (improve x guess) guess)))
+
+(define (sqrt x)
+  (sqrt-iter x 1.0 0))
+
+(define (improve-cb x guess)
+  (/ (+ (* guess 2)
+        (/ x (square guess)))
+     3))
+
+(define (A x y)
+  (cond ((= y 0) 0)
+        ((= x 0) (* 2 y))
+        ((= y 1) 2)
+        (else (A (-  x 1) (A x (- y 1))))))
