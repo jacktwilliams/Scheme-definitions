542615de827d9c9cd6f951dc98faae1803653f33
Jack Williams <JWilliams17@winona.edu>
2018-06-25 15:20:42 -0500

    Completed procedure to find pascals element m at index n.

--- Excersise/PascalsTriangle/find-pascals-element.scm
+++ Excersise/PascalsTriangle/find-pascals-element.scm
@@ -1,12 +1,12 @@
 #lang racket
 (define (pascals index)
-  (define (get-row index)
-    (define (find-row index cur-index row)
+  (define (get-row)
+    (define (find-row cur-index row)
       (define (search-row count)
         (cond ((= count row) -1)
-              ((= (+ cur-index (+ count 1)) index)
+              ((= (+ cur-index count) index)
                (if (or (= count 0)
-                       (= (+ count 1) row))
+                       (= count (- row 1)))
                    -2
                    row))
               (else (search-row (+ count 1))))
@@ -14,16 +14,22 @@
       
       (define result (search-row 0))
       (cond ((= result -1)
-             (find-row index
-                       (+ cur-index row)
+             (find-row (+ cur-index row)
                        (+ row 1)))
             ((= result -2) -1)   
             (else row))
       )
     
     (if (= index 1) -1
-        (find-row index 1 1))
+        (find-row 2 2))
     )
 
+  (define row (get-row))
+  (cond ((= row -1) 1)
+        (else (+ (pascals (- index row))
+                 (pascals (- index (- row 1))))))
   )
+
+
+
   
