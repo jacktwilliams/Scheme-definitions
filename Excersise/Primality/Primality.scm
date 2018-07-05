181d84204706861a4614ab28355fd84bc903cee7
Jack Williams <jwilliams17@winona.edu>
2018-07-04 23:02:16 -0500

    Worked on procedures for finding primes. 1.2.6

---
+++ Excersise/Primality/Primality.scm
@@ -0,0 +1,82 @@
+#lang sicp
+;;below: given in book
+(define (square n)
+  (* n n))
+
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (next test-divisor)))))
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (prime? n)
+(= n (smallest-divisor n)))
+
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (square (expmod base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base (expmod base (- exp 1) m))
+          m))))
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+;;given for excersise 1.22
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time))
+
+;;my procedure for excersise 1.22
+(define (search-for-primes floor ceiling)
+  (define (search n)
+    (timed-prime-test-beauty n)
+    (if (> n ceiling) (display "XXX Ceiling XXX")
+        (search (+ n 2)))
+    )
+  (if (even? floor) (search (+ floor 1))
+      (search floor)))
+
+;;also gonna refactor timed-prime-test for less printing..
+(define (timed-prime-test-beauty n)
+  (define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time)))
+    )
+  (define (report-prime elapsed-time)
+    (newline)
+    (display n)
+    (display " *** ")
+    (display elapsed-time)
+    )
+  (start-prime-test n (runtime)))
+
+;;excersise 1.23 eliminate needless testing
+(define (next test-divisor)
+  (if (= test-divisor 2) 3
+      (+ test-divisor 2)))
+
+
+
