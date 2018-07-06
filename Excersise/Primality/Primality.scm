4f5b7cde56ee709d613378057ede1caa4a57645e
Jack Williams <jwilliams17@winona.edu>
2018-07-05 21:31:59 -0500

    Mostly finished with 1.2

--- Excersise/Primality/Primality.scm
+++ Excersise/Primality/Primality.scm
@@ -8,7 +8,7 @@
 (define (find-divisor n test-divisor)
   (cond ((> (square test-divisor) n) n)
         ((divides? test-divisor n) test-divisor)
-        (else (find-divisor n (next test-divisor)))))
+        (else (find-divisor n (+ test-divisor 1)))))
 
 (define (divides? a b) (= (remainder b a) 0))
 
@@ -62,7 +62,7 @@
 ;;also gonna refactor timed-prime-test for less printing..
 (define (timed-prime-test-beauty n)
   (define (start-prime-test n start-time)
-  (if (prime? n)
+  (if (fast-prime? n 4)
       (report-prime (- (runtime) start-time)))
     )
   (define (report-prime elapsed-time)
@@ -77,6 +77,44 @@
 (define (next test-divisor)
   (if (= test-divisor 2) 3
       (+ test-divisor 2)))
+
+;;excersise 1.27
+(define (inef-prime? n)
+  (define (itr current)
+    (if (= current 1) true
+        (if (= current (expmod current n n))
+            (itr (- current 1)) ;;should test all odds and then 2.
+            false)))
+  (itr (- n 1)))
+
+;;excersise 1.28
+(define (miller-rabin n)
+  (define (new-square a)
+    (define result (* a a))
+    (cond ((= a 1) 1)
+          ((= a (- n 1)) result)
+          ((= result 1) 0)
+          (else result))
+    )
+  (define (expmodded base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder
+          (new-square (expmodded base (/ exp 2) m))
+          m))
+        (else
+         (remainder
+          (* base (expmodded base (- exp 1) m))
+          m))))
+  (define (try-it a)
+    (not (= (expmod a n n) 0))
+    )
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime-smart? n times)
+  (cond ((= times 0) true)
+        ((miller-rabin n) (fast-prime-smart? n (- times 1)))
+        (else false)))
 
 
 
