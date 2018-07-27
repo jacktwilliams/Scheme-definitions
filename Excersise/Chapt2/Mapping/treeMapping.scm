90b5269f6966e3b03f01204d2844a8c22918f7a3
Jack Williams <jwilliams17@winona.edu>
2018-07-27 16:40:11 -0500

    Started working on mapping trees.

---
+++ Excersise/Chapt2/Mapping/treeMapping.scm
@@ -0,0 +1,19 @@
+#lang sicp
+
+(define (scale-tree tree factor)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (* tree factor))
+        (else (cons (scale-tree (car tree) factor)
+                    (scale-tree (cdr tree) factor)))))
+
+;;given above
+;;excersise 2.30 square-tree. Map tree by squaring every leaf
+(define (square-tree tree)
+  (map (lambda (sub-tree)
+         (if (pair? sub-tree)
+             (square-tree sub-tree)
+             (* sub-tree sub-tree)))
+       tree))
+
+;;excersise 2.32. Tree map which takes procedure as argument
+
