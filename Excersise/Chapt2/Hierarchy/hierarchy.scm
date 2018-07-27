3bec58f07e3cfcf3785850d8ba80a43e46340424
Jack Williams <jwilliams17@winona.edu>
2018-07-27 16:23:26 -0500

    Finished work on binary mobiles. Onto 'Mapping over trees'.

--- Excersise/Chapt2/Hierarchy/hierarchy.scm
+++ Excersise/Chapt2/Hierarchy/hierarchy.scm
@@ -19,10 +19,10 @@
 
 ;;2.29 binary mobiles
 (define (make-mobile left right)
-  (list left right))
+  (cons left right))
 
 (define (make-branch length structure)
-  (list length structure))
+  (cons length structure))
 
 (define (left-branch mob)
   (car mob))
@@ -35,14 +35,37 @@
 (define (branch-structure b)
   (cdr b))
 
-(define testb (make-branch 0 1))
-(define testBR (make-branch 1 testb))
-(define testMob (make-mobile testb testBR))
+(define testb (make-branch 1 1))
+(define testBR (make-branch 1 (make-mobile testb testb)))
+(define testMob (make-mobile testBR testBR))
 
 (define (total-weight mob)
   (define (total branch)
-    (if (= (branch-length branch) 0)
-        (car (branch-structure branch))
+    (if (= (branch-length branch) 1)
+        (branch-structure branch)
         (total-weight (branch-structure branch))))
   (+ (total (left-branch mob)) (total (right-branch mob))))
-  
+
+(define (holds-mob? branch)
+  (pair? (branch-structure branch)))
+
+(define (balanced? mob)
+  (let ((left-has (holds-mob? (left-branch mob)))
+        (right-has (holds-mob? (right-branch mob))))
+    (if (and left-has right-has)
+        (and (= (torque (left-branch mob)) (torque (right-branch mob)))
+             (and (balanced? (branch-structure (left-branch mob)))
+                  (balanced? (branch-structure (right-branch mob)))))
+        (if left-has
+            (and (= (torque (left-branch mob)) (torque (right-branch mob)))
+                 (balanced? (branch-structure (left-branch mob))))
+            (if right-has
+                (and (= (torque (left-branch mob)) (torque (right-branch mob)))
+                     (balanced? (branch-structure (right-branch mob))))
+                ;;neither hold mobiles
+                (= (torque (left-branch mob)) (torque (right-branch mob))))))))
+
+(define (torque branch)
+  (if (holds-mob? branch)
+      (* (branch-length branch) (total-weight (branch-structure branch)))
+      (* (branch-structure branch) (branch-length branch))))
