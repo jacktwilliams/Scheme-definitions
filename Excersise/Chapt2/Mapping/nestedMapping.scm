192777f67c232ea1d61f0e82afc7e9e61746cd45
Jack Williams <jwilliams17@winona.edu>
2018-07-30 21:16:01 -0500

    Nested Mapping.

---
+++ Excersise/Chapt2/Mapping/nestedMapping.scm
@@ -0,0 +1,46 @@
+#lang sicp
+; Nested Mapping
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (flatmap
+                           (lambda (i)
+                             (map (lambda (j) (list i j))
+                                  (enumerate-interval 1 (- i 1))))
+                           (enumerate-interval 1 n)))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+;;given are above
+;;2.40 define unique-pairs to simplify prime-sum-pairs
+(define (unique-pairs n)
+  (flatmap
+   (lambda (i)
+     (map (lambda (j) (list i j))
+          (enumerate-interval 1 (- i 1)))
+     (enumerate-interval 1 n))))
--- Excersise/Chapt2/SequenceInterface/seqInterface.scm
+++ Excersise/Chapt2/SequenceInterface/seqInterface.scm
@@ -8,12 +8,6 @@
                (filter predicate (cdr sequence))))
         (else (filter predicate (cdr sequence)))))
 
-(define (accumulate op initial sequence)
-  (if (null? sequence)
-      initial
-      (op (car sequence)
-          (accumulate op initial (cdr sequence)))))
-
 (define (enumerate-interval low high)
   (if (> low high)
       nil
@@ -24,9 +18,9 @@
 ;;2.33. Procedure are given with instruction to fill in blanks to complete basic list
 ;;manipulation operations as accumulations
 
-(define (map p sequence)
-  (accumulate (lambda (x y)
-                (cons (p x) y)) nil sequence))
+;;(define (map p sequence)
+  ;;(accumulate (lambda (x y)
+               ;; (cons (p x) y)) nil sequence))
 
 (define (append seq1 seq2)
   (accumulate cons seq1 seq2))
@@ -38,4 +32,56 @@
 (define test-seq (list 1 2 3 4 5 (list 1 2)))
 (define (square x)
   (* x x))
-(define test-seq1 (list 1 2 3 4 5))
+(define test-seq1 (list 1 2 3 4 5 (list 1 2 3)))
+
+;;2.34 evaluating polynomials using horner's rule
+;; not working correctly..
+(define (horner-eval x coefficient-sequence)
+  (let ((initial (car coefficient-sequence))
+        (seq (cdr coefficient-sequence)))
+    (accumulate (lambda (this-coeff higher-terms)
+                  (if (null? higher-terms)
+                      (+ (* this-coeff x) higher-terms)
+                      (* x (+ this-coeff higher-terms))))
+                initial
+                seq)))
+
+;;2.35 redefining count leaves in terms of accumulate
+(define (count-leaves t)
+  (accumulate
+   (lambda (current rest)
+     (if (pair? current)
+         (+ (count-leaves current) rest)
+         (+ 1 rest)))
+   0
+   t))
+
+;;finishing given definition for accumulate-n. 2.36
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+      nil
+      (cons (accumulate op init (get-firsts seqs))
+            (accumulate-n op init (get-not-firsts seqs)))))
+
+(define (get-firsts seqs)
+  (define (get-first seq)
+    (car seq))
+  (if (null? seqs)
+      nil
+      (cons (get-first (car seqs)) (get-firsts (cdr seqs)))))
+
+(define (get-not-firsts seqs)
+  (define (get-not seq)
+    (cdr seq))
+  (if (null? seqs)
+      nil
+      (cons (get-not (car seqs))
+            (get-not-firsts (cdr seqs)))))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define test-seqs (list (list 1 2 3) (list 1 2 3)))
