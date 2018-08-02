39ff499e0b3fa66f746e8ba73555a23d4126bcd6
Jack Williams <JWilliams17@winona.edu>
2018-08-02 10:22:24 -0500

    Finished excersise with finding triples of ints with a certain sum.

--- Excersise/Chapt2/Mapping/nestedMapping.scm
+++ Excersise/Chapt2/Mapping/nestedMapping.scm
@@ -1,43 +1,10 @@
 #lang sicp
 ; Nested Mapping
-
-(define (flatmap proc seq)
-  (accumulate append nil (map proc seq)))
-
-(define (accumulate op initial sequence)
-  (if (null? sequence)
-      initial
-      (op (car sequence)
-          (accumulate op initial (cdr sequence)))))
-
-(define (prime-sum-pairs n)
-  (map make-pair-sum
-       (filter prime-sum? (flatmap
-                           (lambda (i)
-                             (map (lambda (j) (list i j))
-                                  (enumerate-interval 1 (- i 1))))
-                           (enumerate-interval 1 n)))))
-
-(define (make-pair-sum pair)
-  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
-
-(define (prime-sum? pair)
-  (prime? (+ (car pair) (cadr pair))))
-
-(define (filter predicate sequence)
-  (cond ((null? sequence) nil)
-        ((predicate (car sequence))
-         (cons (car sequence)
-               (filter predicate (cdr sequence))))
-        (else (filter predicate (cdr sequence)))))
-
-(define (enumerate-interval low high)
-  (if (> low high)
-      nil
-      (cons low (enumerate-interval (+ low 1) high))))
-
+; Import prime?
 ;;given are above
 (define (prime? n)
+  (define (square n)
+    (* n n))
   (define (expmod base exp m)
     (cond ((= exp 0) 1)
           ((even? exp)
@@ -72,15 +39,76 @@
         )
       (try-it (+ 1 (random (- n 1)))))
     (cond ((= times 0) true)
-          ((miller-rabin n) (fast-prime-smart? n (- times 1)))
+          ((miller-rabin n) (fast-prime-smart? (- times 1)))
           (else false)))
   (fast-prime-smart? 5))
+
+; Given
+
+(define (flatmap proc seq)
+  (accumulate append nil (map proc seq)))
+
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
+
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+       (filter prime-sum? (unique-pairs n))))
+
+(define (make-pair-sum pair)
+  (list (car pair) (cadr pair) (+ (car pair) (cadr pair))))
+
+(define (prime-sum? pair)
+  (prime? (+ (car pair) (cadr pair))))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+;;given are above
 ;;2.40 define unique-pairs to simplify prime-sum-pairs
 (define (unique-pairs n)
   (flatmap
    (lambda (i)
      (map (lambda (j) (list i j))
-          (enumerate-interval 1 (- i 1)))
-     (enumerate-interval 1 n))))
+          (enumerate-interval 1 (- i 1))))
+     (enumerate-interval 1 n)))
+
+;; 2.41 Write a procedure to find all ordered triples
+;of distinct positive integers i, j, and k less than or equal to
+;a given integer n that sum to a given integer s
+
+(define (unique-triples n)
+  (flatmap
+   (lambda (i)
+     (flatmap (lambda (j)
+            (map (lambda (k)
+                   (list i j k))
+                 (enumerate-interval 1 (- j 1))))
+            (enumerate-interval 1 (- i 1))))
+   (enumerate-interval 3 n)))
+
+(define (make-triple-sum triple)
+  (list (car triple) (cadr triple) (caddr triple)
+        (+ (car triple) (cadr triple) (caddr triple))))
+
+(define (triples-with-sum max-int sum)
+  (map make-triple-sum
+       (filter (lambda (trip)
+                 (= sum (+ (car trip) (cadr trip) (caddr trip))))
+               (unique-triples max-int))))
+       
+          
 
 
