1e6ed9332699e8b0a601d0b45dc71370dca8b787
Jack Williams <jwilliams17@winona.edu>
2018-08-03 13:15:20 -0500

    Added testing artifacts for queens.

--- Excersise/Chapt2/Mapping/queens.scm
+++ Excersise/Chapt2/Mapping/queens.scm
@@ -76,3 +76,16 @@
 (define test4-safe (safe? 2 (adjoin-position 3 2 (adjoin-position 1 1 empty-board)))) ; should pass
 (define sample (adjoin-position 1 3 empty-board))
 (define adv-sample (adjoin-position 2 2 (adjoin-position 1 1 empty-board)))
+; taken from book. producing empty list?
+(define (queen-cols k board-size)
+  (if (= k 0)
+      (list empty-board)
+      (filter
+       (lambda (positions) (safe? k positions))
+       (flatmap
+        (lambda (rest-of-queens)
+          (map (lambda (new-row)
+                 (adjoin-position
+                  new-row k rest-of-queens))
+               (enumerate-interval 1 board-size)))
+        (queen-cols (- k 1) board-size)))))
