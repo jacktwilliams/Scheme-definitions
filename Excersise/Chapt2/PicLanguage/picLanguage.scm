1ce2808493ffa299b317a251c0554117455ed15b
Jack Williams <jwilliams17@winona.edu>
2018-08-08 15:05:36 -0500

    Small work done with picture language.

--- Excersise/Chapt2/PicLanguage/picLanguage.scm
+++ Excersise/Chapt2/PicLanguage/picLanguage.scm
@@ -12,4 +12,35 @@
   (make-vect (+ (xcor v1) (xcor v2)) (+ (ycor v1) (ycor v2))))
 (define (sub-vect v1 v2)
   (make-vect (- (xcor v1) (xcor v2)) (- (ycor v1) (ycor v2))))
+(define (scale-vect x vect)
+  (make-vect (* x (xcor vect)) (* x (ycor vect))))
+
+;excersise 2.47 selectors for two different frame constructors
+(define (make-frame origin edge1 edge2)
+  (list origin edge1 edge2))
+(define (origin frame)
+  (car frame))
+(define (edge1 frame)
+  (cadr frame))
+(define (edge2 frame)
+  (caddr frame))
+
+;(define (make-frame origin edge1 edge2)
+;  (cons origin (cons edge1 edge2)))
+;only needs a new edge 2 selector
+;(define (edge2 frame)
+;  (cddr frame))
+
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (draw-line
+        ((frame-coord-map frame)
+         (start-segment segment))
+        ((frame-coord-map frame)
+         (end-segment segment))))
+     segment-list)))
+
+
 
